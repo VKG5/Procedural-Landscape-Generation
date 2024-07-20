d3511bfb227e352d260726f5d1181d87d6ee8dc2
Lumi_V <bawagupta69@gmail.com>
2024-07-20 23:10:05 +0100

    Added Prompt and Image Path text fields

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
+# Importing required libraries
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit
 from PyQt5.QtGui import QPixmap, QPalette, QColor
 from PyQt5.QtCore import QProcess, Qt
 
+
+# <---------------------- Global Variables ----------------------->
 # Get location of the current file
 directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,11 +15,13 @@ dir = directory.replace('\\', '/')
 
 # Concatenate the path to the executable
 executablePath = dir + '/OpenGL/build/src/Rendering/Debug/Executable.exe'
+imagePath = dir + '/stable-diffusion-webui/outputs/'
 
+# <---------------------- Main Window ----------------------->
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('My GUI with Buttons')
+        self.setWindowTitle('Yumi - Landscape Generator')
         self.setGeometry(100, 100, 400, 300)
         
         # Layout
@@ -27,6 +32,10 @@ class MainWindow(QWidget):
         self.btnGenerateImage.clicked.connect(self.generateImage)
         layout.addWidget(self.btnGenerateImage)
         
+        # Prompt Text Field
+        self.promptTextField = QLineEdit('Enter prompt:')
+        layout.addWidget(self.promptTextField)
+
         # Launch Application Button
         self.btnLaunchApp = QPushButton('Launch Application')
         self.btnLaunchApp.clicked.connect(self.launchApplication)
@@ -38,10 +47,15 @@ class MainWindow(QWidget):
         layout.addWidget(self.btnQuit)
 
         # Image Preview
-        self.imageLabel = QLabel()
-        self.imageLabel.setFixedSize(200, 200)
+        self.imageLabel = QLabel('Image Preview')
+        self.imageLabel.setMaximumSize(2048, 2048)
         layout.addWidget(self.imageLabel)
         
+        # Path Text Field
+        self.pathTextField = QLineEdit('Generated Image Path:')
+        self.pathTextField.setReadOnly(True)
+        layout.addWidget(self.pathTextField)
+
         self.setLayout(layout)
 
         # Pointer to the C++ process
@@ -50,7 +64,10 @@ class MainWindow(QWidget):
     def generateImage(self):
         # Placeholder for image generation logic
         # Here, just display a placeholder image
-        self.imageLabel.setPixmap(QPixmap('path/to/generated/image.png'))
+        self.imageLabel.setPixmap(QPixmap(imagePath + '/img2img-images/2024-06-25/00000-1723017853.png'))
+        self.imageLabel.setAlignment(Qt.AlignCenter)
+
+        self.pathTextField.setText(imagePath + 'img2img-images/2024-06-25/00000-1723017853.png')
     
     def quitApp(self):
         # Quit the launched application if it is running
@@ -71,21 +88,23 @@ class MainWindow(QWidget):
     def launchApplication(self):
         # If process is already running, do not start another instance
         if self.process and self.process.state() == QProcess.Running:
-            print("Application is already running.")
+            print("C++ Application is already running.")
             return
 
         ## Debugging
-        print(f"Starting application from : \"{executablePath}\"")
+        print(f"Starting C++ application from : \"{executablePath}\"")
         
         self.process = QProcess()
         self.process.start(executablePath)
 
         if self.process.waitForStarted():
-            print(f"Application started with PID : {self.process.processId()}")
+            print(f"C++ Application started with PID : {self.process.processId()}")
         
         else:
-            print("Failed to start application.")
+            print("Failed to start C++ application.")
 
+
+# <---------------------- Main Application ----------------------->
 def main():
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
@@ -113,5 +132,7 @@ def main():
     window.show()
     sys.exit(app.exec_())
 
+
+# <---------------------- Entry Point ----------------------->
 if __name__ == '__main__':
     main()
